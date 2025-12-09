d25636cb3258f3b7528d2ebdc95fce9168f86868
lujainjumaa2@gmail.com <lujainjumaa2@gmail.com>
2025-12-09 12:18:28 +0300

    fix name of apartmentRentalsController

--- app/Http/Controllers/ApartmentRentalsController.php
+++ app/Http/Controllers/ApartmentRentalsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ApartmentRentals extends Controller
+class ApartmentRentalsController extends Controller
 {
     //rental logic here
     // e.g., create rental, view rentals user id, edit rental ,remove retal.
